6f9f15ea948a9eb85d64b2e0356206abb4213578
unaayy <unaayy@gmail.com>
2026-09-10 02:28:42 +0200

    feat: sitemap para lidunax

---
+++ src/pages/sitemap.xml.js
@@ -0,0 +1,21 @@
+export async function getServerSideProps({ res }) {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://www.lidunax.com/</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+    <changefreq>daily</changefreq>
+    <priority>1.0</priority>
+  </url>
+</urlset>`;
+
+  res.setHeader('Content-Type', 'text/xml');
+  res.write(sitemap);
+  res.end();
+
+  return { props: {} };
+}
+
+export default function Sitemap() {
+  return null;
+}
