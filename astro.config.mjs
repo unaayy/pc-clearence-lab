dfc5e131995fc1d07883e6513e2463e209bcb3f4
unaayy <unaayy@gmail.com>
2026-09-11 18:33:05 +0200

    fix: instalar sitemap exitosamente y configurar dominio correcto

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://www.gridcube.com',
+  site: 'https://www.lidunax.com',
   integrations: [
     tailwind(),
     react(),
